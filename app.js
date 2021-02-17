f665e31958e0b9421cd33bc59aef707120958d46
Christos Topaloudis <topless@gmail.com>
2021-02-17 22:11:30 +0100

    Middleware for raw body data and removed some ugliness

--- app.js
+++ app.js
@@ -4,8 +4,28 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.static('static'));
+app.use('/static', express.static('static'));
+
+const rawDataMiddleWare = (req, res, next) => {
+  var data = '';
+  req.setEncoding('utf8');
+  req.on('data', (chunk) => (data += chunk));
+  req.on('end', () => {
+    req.body = data;
+    next();
+  });
+};
+
+app.use(rawDataMiddleWare);
+
 app.get('/', (req, res) => {
   res.status(200).sendFile('index.html', { root: __dirname });
+});
+
+app.post('/', (req, res) => {
+  console.log(req.body);
+  res.sendStatus(200);
 });
 
 const PORT = process.env.PORT || 8080;
